dcb6d6c248dd08432f61a9de4924b31a73916cce
taiyo <taiyo.yuujin@gmail.com>
2017-10-11 01:53:01 +0900

    Create code
    Code to find maximum value, minimum value, average value, median

---
+++ calc.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+filename = input("ファイル名を入力してください ")
+f = open(filename)
+
+z = []
+for i in f:
+    xyz = i.split()
+    if xyz[2]!='-9999.99':
+        xyz2 = float(xyz[2])
+        z.append(xyz2)
+
+print("最大値:",max(z))
+print("最小値:",min(z))
+print("平均値:",np.mean(z))
+print("中央値:",np.median(z))
